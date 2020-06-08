53c368ee980b682374b9d4f1f07445dcae3a72d6
emmateva18 <emmateva18@codingburgas.bg>
2020-06-08 21:44:19 +0300

    Main .cpp file

--- string project/string project.cpp
+++ string project/string project.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include <string>
+#include "Header.h";
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int Flag = 0;
+	do
+	{
+		Menu(Flag);
+	} while (Flag == 0);
 }
