dfd32e68a1838f8405604342dac2a031f60ec3ff
dmgeorgiev18 <dmgeorgiev18@codingburgas.bg>
2020-06-10 21:12:35 +0300

    add Medium level

--- string project/Words.cpp
+++ string project/Words.cpp
@@ -11,3 +11,12 @@ void wordsEasy()
         cout << i << ". " << wordsE[i] << endl;
     }
 }
+void wordsMedium()
+{
+    string wordsM[19] = { "","Attitude","Harmony","Love","Peace","Powerful","Pure","Receptive","Success","Will-Power",
+        "Freedom","High-Energy","Motivation","Positive","Progress","Quality","Self-Discovery","Victory","Yourself" };
+    for (int i = 1; i < 19; i++)
+    {
+        cout << i << ". " << wordsM[i] << endl;
+    }
+}
