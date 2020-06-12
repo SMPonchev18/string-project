e28eb2774ff258bcd5288ca339ddf39a12aa2009
dpdyakov18 <dpdyakov18@codingburgas.bg>
2020-06-13 00:02:14 +0300

    Minor changes

--- string project/Header.h
+++ string project/Header.h
@@ -1,11 +1,14 @@
 #pragma once
 bool Menu(int& Flag);
+//boards for the different levels
 int easyDifficultyBoard();
 int mediumDifficultyBoard();
 int hardDifficultyBoard();
+//words for the different levels
 void wordsEasy();
 void wordsMedium();
 void wordsHard();
+//searching the different words
 int searchEasy();
 int searchMedium();
 int searchHard();
